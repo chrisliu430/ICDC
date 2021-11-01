03b1586f161247e6f33fab313b2912e90c47da08
chrisliu430 <hearnechris430@gmail.com>
2021-11-01 21:27:52 +0800

    Fix

--- 2021_E_Pre/geofence.v
+++ 2021_E_Pre/geofence.v
@@ -17,14 +17,11 @@ module geofence (clk, reset, X, Y, valid, is_inside);
     reg [2:0] nextStatus;
     reg [1:0] countNum;
 
-    reg signed [19:0] xA, yA, xB, yB;
-
     wire [20:0] outPot;
 
     reg [9:0] targetX, targetY;
     reg [9:0] tempX [5:0];
     reg [9:0] tempY [5:0];
-    reg [9:0] swap;
 
     assign status = nextStatus;
 
@@ -41,91 +38,100 @@ module geofence (clk, reset, X, Y, valid, is_inside);
             countTemp <= 0;
             nextStatus <= 0;
         end else begin
-            if (status == 0) begin
-                _valid <= 0;
-                targetX <= X;
-                targetY <= Y;
-                nextStatus <= 1;
-                countResult <= 0;
-                countIndex <= 0;
-            end else if (status == 1) begin
-                if (countTemp == 6) begin
-                    nextStatus <= 2;
-                    countNum <= 0;
-                end else begin
-                    tempX[0] <= tempX[1];
-                    tempX[1] <= tempX[2];
-                    tempX[2] <= tempX[3];
-                    tempX[3] <= tempX[4];
-                    tempX[4] <= tempX[5];
-                    tempX[5] <= X;
-                    tempY[0] <= tempY[1];
-                    tempY[1] <= tempY[2];
-                    tempY[2] <= tempY[3];
-                    tempY[3] <= tempY[4];
-                    tempY[4] <= tempY[5];
-                    tempY[5] <= Y;
-                    countTemp <= countTemp + 1;
+            case (status)
+                3'd0: begin
+                    _valid <= 0;
+                    targetX <= X;
+                    targetY <= Y;
+                    nextStatus <= 1;
+                    countResult <= 0;
+                    countIndex <= 0;
                 end
-            end else if (status == 2) begin
-                if (outPot[20] == 1) begin
-                    nextStatus <= 3;
-                    countNum <= countNum + 1;
-                end else begin
-                    tempX[1] <= tempX[2];
-                    tempX[2] <= tempX[1];
-                    tempY[1] <= tempY[2];
-                    tempY[2] <= tempY[1];
+                3'd1: begin
+                    if (countTemp == 6) begin
+                        nextStatus <= 2;
+                        countNum <= 0;
+                    end else begin
+                        tempX[0] <= tempX[1];
+                        tempX[1] <= tempX[2];
+                        tempX[2] <= tempX[3];
+                        tempX[3] <= tempX[4];
+                        tempX[4] <= tempX[5];
+                        tempX[5] <= X;
+                        tempY[0] <= tempY[1];
+                        tempY[1] <= tempY[2];
+                        tempY[2] <= tempY[3];
+                        tempY[3] <= tempY[4];
+                        tempY[4] <= tempY[5];
+                        tempY[5] <= Y;
+                        countTemp <= countTemp + 1;
+                    end
                 end
-            end else if (status == 3) begin
-                if (outPot[20] == 1) begin
-                    nextStatus <= 4;
-                    countNum <= countNum + 1;
-                end else begin
-                    tempX[2] <= tempX[3];
-                    tempX[3] <= tempX[2];
-                    tempY[2] <= tempY[3];
-                    tempY[3] <= tempY[2];
-                    countNum <= 0;
-                    nextStatus <= 2;
+                3'd2: begin
+                    if (outPot[20] == 1) begin
+                        nextStatus <= 3;
+                        countNum <= countNum + 1;
+                    end else begin
+                        tempX[1] <= tempX[2];
+                        tempX[2] <= tempX[1];
+                        tempY[1] <= tempY[2];
+                        tempY[2] <= tempY[1];
+                    end
                 end
-            end else if (status == 4) begin
-                if (outPot[20] == 1) begin
-                    nextStatus <= 5;
-                    countNum <= countNum + 1;
-                end else begin
-                    tempX[3] <= tempX[4];
-                    tempX[4] <= tempX[3];
-                    tempY[3] <= tempY[4];
-                    tempY[4] <= tempY[3];
-                    countNum <= 0;
-                    nextStatus <= 2;
+                3'd3: begin
+                    if (outPot[20] == 1'b1) begin
+                        nextStatus <= 4;
+                        countNum <= countNum + 1;
+                    end else begin
+                        tempX[2] <= tempX[3];
+                        tempX[3] <= tempX[2];
+                        tempY[2] <= tempY[3];
+                        tempY[3] <= tempY[2];
+                        countNum <= 0;
+                        nextStatus <= 2;
+                    end
                 end
-            end else if (status == 5) begin
-                if (outPot[20] == 1) begin
-                    nextStatus <= 6;
-                end else begin
-                    tempX[4] <= tempX[5];
-                    tempX[5] <= tempX[4];
-                    tempY[4] <= tempY[5];
-                    tempY[5] <= tempY[4];
-                    countNum <= 0;
-                    nextStatus <= 2;
+                3'd4: begin
+                    if (outPot[20] == 1'b1) begin
+                        nextStatus <= 5;
+                        countNum <= countNum + 1;
+                    end else begin
+                        tempX[3] <= tempX[4];
+                        tempX[4] <= tempX[3];
+                        tempY[3] <= tempY[4];
+                        tempY[4] <= tempY[3];
+                        countNum <= 0;
+                        nextStatus <= 2;
+                    end
                 end
-            end else if (status == 6) begin
-                if (countIndex == 5) begin
-                    _valid <= 1;
-                    _inside <= (countResult == 5) ? 1 : 0;
-                    nextStatus <= 7;
-                end else begin
-                    countIndex <= countIndex + 1;
-                    countResult <= (target[20] == compare[20]) ? countResult + 1 : countResult;
+                3'd5: begin
+                    if (outPot[20] == 1'b1) begin
+                        nextStatus <= 6;
+                    end else begin
+                        tempX[4] <= tempX[5];
+                        tempX[5] <= tempX[4];
+                        tempY[4] <= tempY[5];
+                        tempY[5] <= tempY[4];
+                        countNum <= 0;
+                        nextStatus <= 2;
+                    end
                 end
-            end else begin
-                _valid <= 0;
-                nextStatus <= 0;
-                countTemp <= 0;
-            end
+                3'd6: begin
+                    if (countIndex == 3'b101) begin
+                        _valid <= 1;
+                        _inside <= (countResult == 5) ? 1 : 0;
+                        nextStatus <= 7;
+                    end else begin
+                        countIndex <= countIndex + 1;
+                        countResult <= (target[20] == compare[20]) ? countResult + 1 : countResult;
+                    end
+                end
+                default: begin
+                    _valid <= 0;
+                    nextStatus <= 0;
+                    countTemp <= 0;
+                end
+            endcase
         end
     end
     
