511cf78d2778c3a3f857b5514df18841e9667e7b
chrisliu430 <hearnechris430@gmail.com>
2021-11-02 12:02:16 +0800

    Update

--- 2021_E_Pre/geofence.v
+++ 2021_E_Pre/geofence.v
@@ -6,8 +6,11 @@ module geofence (clk, reset, X, Y, valid, is_inside);
     output valid;
     output is_inside;
 
+    integer i;
+
+    wire target;
     wire [2:0] status;
-    wire [20:0] target, compare;
+    wire [20:0] compare;
     reg [2:0] countIndex;
     reg [2:0] countResult;
 
@@ -15,9 +18,9 @@ module geofence (clk, reset, X, Y, valid, is_inside);
 
     reg [2:0] countTemp;
     reg [2:0] nextStatus;
-    reg [1:0] countNum;
 
     wire [20:0] outPot;
+    reg [20:0] cal;
 
     reg [9:0] targetX, targetY;
     reg [9:0] tempX [5:0];
@@ -25,10 +28,10 @@ module geofence (clk, reset, X, Y, valid, is_inside);
 
     assign status = nextStatus;
 
-    assign outPot = (tempX[countNum + 1] - tempX[0]) * (tempY[countNum + 2] - tempY[0]) - (tempX[countNum + 2] - tempX[0]) * (tempY[countNum + 1] - tempY[0]);
+    assign outPot = (tempX[countIndex + 1] - tempX[0]) * (tempY[countIndex + 2] - tempY[0]) - (tempX[countIndex + 2] - tempX[0]) * (tempY[countIndex + 1] - tempY[0]);
 
     assign compare = (tempX[countIndex] - targetX) * (tempY[countIndex + 1] - tempY[countIndex]) - (tempX[countIndex + 1] - tempX[countIndex]) * (tempY[countIndex] - targetY);
-    assign target = (tempX[5] - targetX) * (tempY[0] - tempY[5]) - (tempX[0] - tempX[5]) * (tempY[5] - targetY);
+    assign target = cal[20];
 
     assign valid = _valid;
     assign is_inside = _inside;
@@ -45,24 +48,17 @@ module geofence (clk, reset, X, Y, valid, is_inside);
                     targetY <= Y;
                     nextStatus <= 1;
                     countResult <= 0;
-                    countIndex <= 0;
                 end
                 3'd1: begin
                     if (countTemp == 6) begin
                         nextStatus <= 2;
-                        countNum <= 0;
+                        countIndex <= 0;
                     end else begin
-                        tempX[0] <= tempX[1];
-                        tempX[1] <= tempX[2];
-                        tempX[2] <= tempX[3];
-                        tempX[3] <= tempX[4];
-                        tempX[4] <= tempX[5];
+                        for (i = 0; i < 5; i = i + 1) begin
+                            tempX[i] <= tempX[i + 1];
+                            tempY[i] <= tempY[i + 1];
+                        end
                         tempX[5] <= X;
-                        tempY[0] <= tempY[1];
-                        tempY[1] <= tempY[2];
-                        tempY[2] <= tempY[3];
-                        tempY[3] <= tempY[4];
-                        tempY[4] <= tempY[5];
                         tempY[5] <= Y;
                         countTemp <= countTemp + 1;
                     end
@@ -70,7 +66,7 @@ module geofence (clk, reset, X, Y, valid, is_inside);
                 3'd2: begin
                     if (outPot[20] == 1) begin
                         nextStatus <= 3;
-                        countNum <= countNum + 1;
+                        countIndex <= countIndex + 1;
                     end else begin
                         tempX[1] <= tempX[2];
                         tempX[2] <= tempX[1];
@@ -81,49 +77,51 @@ module geofence (clk, reset, X, Y, valid, is_inside);
                 3'd3: begin
                     if (outPot[20] == 1'b1) begin
                         nextStatus <= 4;
-                        countNum <= countNum + 1;
+                        countIndex <= countIndex + 1;
                     end else begin
                         tempX[2] <= tempX[3];
                         tempX[3] <= tempX[2];
                         tempY[2] <= tempY[3];
                         tempY[3] <= tempY[2];
-                        countNum <= 0;
+                        countIndex <= 0;
                         nextStatus <= 2;
                     end
                 end
                 3'd4: begin
                     if (outPot[20] == 1'b1) begin
                         nextStatus <= 5;
-                        countNum <= countNum + 1;
+                        countIndex <= countIndex + 1;
                     end else begin
                         tempX[3] <= tempX[4];
                         tempX[4] <= tempX[3];
                         tempY[3] <= tempY[4];
                         tempY[4] <= tempY[3];
-                        countNum <= 0;
+                        countIndex <= 0;
                         nextStatus <= 2;
                     end
                 end
                 3'd5: begin
                     if (outPot[20] == 1'b1) begin
                         nextStatus <= 6;
+                        cal <= (tempX[5] - targetX) * (tempY[0] - tempY[5]) - (tempX[0] - tempX[5]) * (tempY[5] - targetY);
+                        countIndex <= 0;
                     end else begin
                         tempX[4] <= tempX[5];
                         tempX[5] <= tempX[4];
                         tempY[4] <= tempY[5];
                         tempY[5] <= tempY[4];
-                        countNum <= 0;
+                        countIndex <= 0;
                         nextStatus <= 2;
                     end
                 end
                 3'd6: begin
                     if (countIndex == 3'b101) begin
                         _valid <= 1;
-                        _inside <= (countResult == 5) ? 1 : 0;
+                        _inside <= (countResult == 3'b101) ? 1 : 0;
                         nextStatus <= 7;
                     end else begin
                         countIndex <= countIndex + 1;
-                        countResult <= (target[20] == compare[20]) ? countResult + 1 : countResult;
+                        countResult <= (target == compare[20]) ? countResult + 1 : countResult;
                     end
                 end
                 default: begin
@@ -134,6 +132,6 @@ module geofence (clk, reset, X, Y, valid, is_inside);
             endcase
         end
     end
-    
+
 endmodule
 
